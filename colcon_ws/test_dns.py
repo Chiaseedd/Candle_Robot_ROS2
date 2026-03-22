c25f477c82dbf922b6dfa1f66691fbfeeba08de6
Chiaseedd <138270183+Chiaseedd@users.noreply.github.com>
2026-03-22 14:04:15 +0800

    Delete colcon_ws/test_dns.py

--- colcon_ws/test_dns.py
+++
@@ -1,8 +0,0 @@
-import socket
-import time
-t = time.time()
-try:
-    socket.getaddrinfo(socket.gethostname(), 0, socket.AF_UNSPEC, socket.SOCK_STREAM)
-except Exception as e:
-    print(f"Error: {e}")
-print(f"Time: {time.time() - t}")
